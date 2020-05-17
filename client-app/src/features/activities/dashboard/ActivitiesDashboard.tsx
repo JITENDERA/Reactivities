9e735e9e6093015084ae014da0ca70de239416b2
Jitendera Pratap Singh <JITENDERA@users.noreply.github.com>
2020-05-17 18:41:50 -0500

    End of Section 8

--- client-app/src/features/activities/dashboard/ActivitiesDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivitiesDashboard.tsx
@@ -1,28 +1,26 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Grid } from "semantic-ui-react";
 import ActivityList from "./ActivityList";
-import ActivitiesDetails from "../details/ActivitiesDetails";
-import ActivityForm from "../form/ActivityForm";
 import { observer } from "mobx-react-lite";
-import ActivityStore from "../../../app/stores/activityStore";
+import { LoadingComponent } from "../../../app/layout/LoadingComponent";
+import ActivityStore from '../../../app/stores/activityStore'
 
 const ActivitiesDashboard: React.FC = () => {
   const activityStore = useContext(ActivityStore);
-  const { editMode, selectedActivity } = activityStore;
 
+  useEffect(() => {
+    activityStore.loadingActivities();
+  }, [activityStore]);
+
+  if (activityStore.loadingInitial)
+    return <LoadingComponent content="Loading activities..." />;
   return (
     <Grid>
       <Grid.Column width={10}>
         <ActivityList />
       </Grid.Column>
       <Grid.Column width={6}>
-        {selectedActivity && !editMode && <ActivitiesDetails />}
-        {editMode && (
-          <ActivityForm
-            key={(selectedActivity && selectedActivity.id) || 0}
-            activity={selectedActivity!}
-          />
-        )}
+        <h2>Activity filters</h2>
       </Grid.Column>
     </Grid>
   );
